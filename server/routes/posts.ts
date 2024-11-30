e378247f0e74f0a1ac1a5c3d71005275d8ef85f0
Sangeeth Sivan <berzerkeer@pm.me>
2024-11-30 20:19:09 +0530

    feat: fetch posts, pagination support and debug flag for drizzle queries for now

--- server/routes/posts.ts
+++ server/routes/posts.ts
@@ -1,20 +1,27 @@
 import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
+import { and, asc, countDistinct, desc, eq, sql } from "drizzle-orm";
 
 import { zValidator } from "@hono/zod-validator";
 import { db } from "~/adapter.ts";
 import type { Context } from "~/context.ts";
+import { userTable } from "~/db/schemas/auth.ts";
 import { postsTable } from "~/db/schemas/posts.ts";
+import { postUpvotesTable } from "~/db/schemas/upvotes.ts";
 import { loggedIn } from "~/middleware/loggedIn.ts";
 import postgres from "postgres";
 
-import { createPostSchema, type SuccessResponse } from "~/shared/types.ts";
+import {
+  createPostSchema,
+  paginationSchema,
+  type PaginatedResponse,
+  type Post,
+  type SuccessResponse,
+} from "~/shared/types.ts";
+import { getISOFormatDateQuery } from "~/lib/utils.ts";
 
-export const postsRouter = new Hono<Context>().post(
-  "/",
-  loggedIn,
-  zValidator("form", createPostSchema),
-  async (c) => {
+export const postsRouter = new Hono<Context>()
+  .post("/", loggedIn, zValidator("form", createPostSchema), async (c) => {
     const { title, url, content } = c.req.valid("form");
     const user = c.get("user")!;
     try {
@@ -42,5 +49,78 @@ export const postsRouter = new Hono<Context>().post(
         throw new HTTPException(500, { message: "Failed to create post" });
       }
     }
-  },
-);
+  })
+  .get("/", zValidator("query", paginationSchema), async (c) => {
+    const { limit, page, site, author, order, sortBy } = c.req.valid("query");
+    const user = c.get("user");
+
+    const offset = (page - 1) * limit;
+
+    const sortByColumn =
+      sortBy === "points" ? postsTable.points : postsTable.createdAt;
+    const sortOrder = order === "desc" ? desc(sortByColumn) : asc(sortByColumn);
+
+    const [count] = await db
+      .select({ count: countDistinct(postsTable.id) })
+      .from(postsTable)
+      .where(
+        and(
+          author ? eq(postsTable.userId, author) : undefined,
+          site ? eq(postsTable.url, site) : undefined,
+        ),
+      );
+
+    const postsQuery = db
+      .select({
+        id: postsTable.id,
+        title: postsTable.title,
+        url: postsTable.url,
+        points: postsTable.points,
+        content: postsTable.content,
+        createdAt: getISOFormatDateQuery(postsTable.createdAt),
+        commentCount: postsTable.commentCount,
+        author: {
+          username: userTable.username,
+          id: userTable.id,
+        },
+        isUpvoted: user
+          ? sql<boolean>`CASE WHEN ${postUpvotesTable.userId} IS NOT NULL THEN true ELSE false END`
+          : sql<boolean>`${false}`,
+      })
+      .from(postsTable)
+      .leftJoin(userTable, eq(postsTable.userId, userTable.id))
+      .orderBy(sortOrder)
+      .limit(limit)
+      .offset(offset)
+      .where(
+        and(
+          author ? eq(postsTable.userId, author) : undefined,
+          site ? eq(postsTable.url, site) : undefined,
+        ),
+      );
+
+    if (user) {
+      postsQuery.leftJoin(
+        postUpvotesTable,
+        and(
+          eq(postUpvotesTable.postId, postsTable.id),
+          eq(postUpvotesTable.userId, user.id),
+        ),
+      );
+    }
+
+    const posts = await postsQuery;
+
+    return c.json<PaginatedResponse<Post[]>>(
+      {
+        data: posts as Post[],
+        success: true,
+        message: "Posts fetched",
+        pagination: {
+          page,
+          totalPages: Math.ceil(count.count / limit),
+        },
+      },
+      200,
+    );
+  });
--- shared/types.ts
+++ shared/types.ts
@@ -31,3 +31,40 @@ export const createPostSchema = insertPostSchema
     message: "URL or Content must be provided",
     path: ["url", "content"],
   });
+
+export const sortBySchema = z.enum(["recent", "points"]);
+export const orderBySchema = z.enum(["asc", "desc"]);
+
+export const paginationSchema = z.object({
+  limit: z.number({ coerce: true }).default(10),
+  page: z.number({ coerce: true }).default(1),
+  sortBy: sortBySchema.optional().default("points"),
+  order: orderBySchema.optional().default("asc"),
+  author: z.string().optional(),
+  site: z.string().optional(),
+});
+
+export type Author = {
+  id: string;
+  username: string;
+};
+
+export type Post = {
+  id: number;
+  title: string;
+  url: string | null;
+  points: number;
+  createdAt: string;
+  content: string | null;
+  commentCount: number;
+  author: Author;
+  isUpvoted: boolean;
+};
+
+export type PaginatedResponse<T = void> = {
+  pagination: {
+    page: number;
+    totalPages: number;
+  };
+  data: T;
+} & Omit<SuccessResponse, "data">;
