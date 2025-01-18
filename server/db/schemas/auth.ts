f4cbae7e8bee63e17c926f1234232cd120e766d0
Sangeeth Sivan <berzerkeer@pm.me>
2025-01-18 12:24:57 +0530

    feat: added user relations

--- server/db/schemas/auth.ts
+++ server/db/schemas/auth.ts
@@ -1,11 +1,27 @@
 import { text, timestamp } from "drizzle-orm/pg-core";
 import { pgTable } from "drizzle-orm/pg-core/table";
+import { relations } from "drizzle-orm/relations";
+
+import { commentsTable } from "~/db/schemas/comments.ts";
+import { postsTable } from "~/db/schemas/posts.ts";
+import { commentUpvotesTable, postUpvotesTable } from "~/db/schemas/upvotes.ts";
 
 export const userTable = pgTable("user", {
   id: text("id").primaryKey(),
   username: text("username").notNull().unique(),
   password_hash: text("password_hash").notNull(),
 });
+
+export const userRelations = relations(userTable, ({ many }) => ({
+  posts: many(postsTable, { relationName: "author" }),
+  comments: many(commentsTable, { relationName: "author" }),
+  postUpvotes: many(postUpvotesTable, {
+    relationName: "postUpvotes",
+  }),
+  commentUpvotes: many(commentUpvotesTable, {
+    relationName: "commentUpvotes",
+  }),
+}));
 
 export const sessionTable = pgTable("session", {
   id: text("id").primaryKey(),
